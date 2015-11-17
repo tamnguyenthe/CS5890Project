1ec642ae7aeef9396af664ae4e0183707ae2c8f3
Tam Nguyen <tamnguyen.usu@gmail.com>
2015-11-17 13:54:12 -0700

    add java project

---
+++ java/src/usu/useal/Main.java
@@ -0,0 +1,14 @@
+package usu.useal;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String rootDir = "/Users/tamnguyen/Documents/PycharmProjects/CS5890Project/data";
+        TextAnalyzer textAnalyzer = new TextAnalyzer( rootDir + File.separator + "train.json", rootDir+ File.separator + "test.json");
+        textAnalyzer.loadStopWords(rootDir + File.separator + "english.stop");
+        System.out.printf("anh yeu em");
+
+    }
+}
---
+++ java/src/usu/useal/TextAnalyzer.java
@@ -0,0 +1,16 @@
+package usu.useal;
+
+/**
+ * Created by tamnguyen on 11/17/15.
+ */
+public class TextAnalyzer {
+
+    public TextAnalyzer(String trainFileName, String testFileName) {
+
+    }
+
+
+    public void loadStopWords(String fileName) {
+
+    }
+}
